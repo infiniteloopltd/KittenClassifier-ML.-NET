abf957d415139558dc5650094f895712e17245eb
Infinite Loop Development Ltd <info@infiniteloop.ie>
2023-02-21 19:05:11 +0100

    Compiles ... but does it work

--- ML.cs
+++ ML.cs
@@ -1,72 +1,57 @@
-﻿using Microsoft.ML;
-using Microsoft.ML.Data;
+﻿using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.IO;
+using System.Linq;
+using Accord.IO;
+using Accord.MachineLearning.VectorMachines.Learning;
+using Accord.Math;
+using Accord.Statistics.Kernels;
 
-namespace MultiOutputModelTraining
+namespace AccordNETExample
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var mlContext = new MLContext();
+            // Read the CSV file
+            string filePath = @"kittens.csv";
+            var fileContents = File.ReadAllText(filePath);
+            var data = CsvReader.FromText(fileContents, hasHeaders: true).ToTable();
+            // Prepare the inputs and outputs
 
-            // Load data
-            IDataView dataView = mlContext.Data.LoadFromTextFile<InputData>("data.csv", separatorChar: ',', hasHeader: true);
+  
+            double[][] inputs = data.Columns["Code"].ToArray<string>().Select(c => new[] { (double)c.ToString().GetHashCode() }).ToArray();
+            int[] outputsOrange = data.Columns["IsOrange"].ToArray<string>().Select(c => c.ToString().ToLower() == "true" ? 1 : 0).ToArray();
+            int[] outputsGrey = data.Columns["IsGrey"].ToArray<string>().Select(c => c.ToString().ToLower() == "true" ? 1 : 0).ToArray();
+            int[] outputsMale = data.Columns["IsMale"].ToArray<string>().Select(c => c.ToString().ToLower() == "true" ? 1 : 0).ToArray();
 
-            // Split data into training and testing sets
-            var dataSplit = mlContext.Data.TrainTestSplit(dataView, testFraction: 0.2);
-            var trainData = dataSplit.TrainSet;
-            var testData = dataSplit.TestSet;
+            // Train the models
+            var teacherOrange = new SequentialMinimalOptimization<Gaussian>()
+            {
+                Complexity = 100
+            };
+            var modelOrange = teacherOrange.Learn(inputs, outputsOrange);
 
-            // Define data preprocessing pipeline
-            var dataPipeline = mlContext.Transforms.Conversion.MapValueToKey("LabelOrange", "IsOrange")
-                                                 .Append(mlContext.Transforms.Conversion.MapValueToKey("LabelGrey", "IsGrey"))
-                                                 .Append(mlContext.Transforms.Conversion.MapValueToKey("LabelMale", "IsMale"))
-                                                 .Append(mlContext.Transforms.Categorical.OneHotEncoding("CodeEncoded", "Code"))
-                                                 .Append(mlContext.Transforms.Concatenate("Features", "CodeEncoded"))
-                                                 .Append(mlContext.Transforms.Conversion.ConvertType("FeaturesFloat", "Features"));
+            var teacherGrey = new SequentialMinimalOptimization<Gaussian>()
+            {
+                Complexity = 100
+            };
+            var modelGrey = teacherGrey.Learn(inputs, outputsGrey);
 
-            // Define training pipeline
-            var trainingPipeline = dataPipeline.Append(mlContext.Transforms.Concatenate("NonKeyOutput", "LabelOrange", "LabelGrey", "LabelMale"))
-                                               .Append(mlContext.Transforms.Conversion.MapKeyToValue("Orange", "Orange"))
-                                               .Append(mlContext.Transforms.Conversion.MapKeyToValue("Grey", "Grey"))
-                                               .Append(mlContext.Transforms.Conversion.MapKeyToValue("Male", "Male"))
-                                               .Append(mlContext.Transforms.Concatenate("KeyOutput", "Orange", "Grey", "Male"))
-                                               .Append(mlContext.Transforms.CopyColumns("Label", "KeyOutput"))
-                                               .Append(mlContext.Transforms.NormalizeMinMax("FeaturesFloat", "FeaturesFloat"))
-                                               .Append(mlContext.BinaryClassification.Trainers.SdcaNonCalibrated("Model", maximumNumberOfIterations: 100));
+            var teacherMale = new SequentialMinimalOptimization<Gaussian>()
+            {
+                Complexity = 100
+            };
+            var modelMale = teacherMale.Learn(inputs, outputsMale);
 
-            // Train the model
-            var trainedModel = trainingPipeline.Fit(trainData);
-
-            // Evaluate the model
-            var predictions = trainedModel.Transform(testData);
-            var metrics = mlContext.MulticlassClassification.Evaluate(predictions, labelColumnName: "Label", scoreColumnName: "Model");
-
-            Console.WriteLine($"Accuracy: {metrics.MacroAccuracy}");
+            // Save the models
+            string modelFilePath = @"path/to/save/your/models/";
+            if (!Directory.Exists(modelFilePath))
+                Directory.CreateDirectory(modelFilePath);
+            Serializer.Save(modelOrange, Path.Combine(modelFilePath, "modelOrange.bin"));
+            Serializer.Save(modelGrey, Path.Combine(modelFilePath, "modelGrey.bin"));
+            Serializer.Save(modelMale, Path.Combine(modelFilePath, "modelMale.bin"));
         }
-    }
-
-    public class InputData
-    {
-        [LoadColumn(0)]
-        public string Code;
-
-        [LoadColumn(1)]
-        public bool IsOrange;
-
-        [LoadColumn(2)]
-        public bool IsGrey;
-
-        [LoadColumn(3)]
-        public bool IsMale;
-    }
-
-    public class OutputData
-    {
-        public bool Orange;
-
-        public bool Grey;
-
-        public bool Male;
     }
 }
